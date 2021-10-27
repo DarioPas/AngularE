bcfed0e523865bf90cbccf46a808278d587e488c
Dario <dariopas@live.it>
2021-10-27 11:52:53 +0200

    consegna

---
+++ AngularE2/src/app/user-details/User.ts
@@ -0,0 +1,26 @@
+export interface User {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  dateOfBirth: string;
+  address: Address;
+  role: Role;
+  username: string;
+  profilePhotoUrl: string;
+  companies: Company[];
+  gender: Gender;
+};
+type Role = 'staff' | 'student' | 'manager' | 'admin';
+type Gender = 'male' | 'female' | 'other';
+interface Company {
+  id?: number;
+  name: string;
+  description: string;
+  location?: Address;
+};
+interface Address {
+  city: string;
+  street: string;
+  postalCode: string;
+}
